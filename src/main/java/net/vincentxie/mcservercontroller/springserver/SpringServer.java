cf7facbbe2f5f31f397efb23cedb0d820e8b4a05
zbrachinara <zbrachinara@gmail.com>
2020-07-21 15:10:06 -0400

    Add spring imports

--- src/main/java/net/vincentxie/mcservercontroller/springserver/SpringServer.java
+++ src/main/java/net/vincentxie/mcservercontroller/springserver/SpringServer.java
@@ -1,4 +1,13 @@
 package net.vincentxie.mcservercontroller.springserver;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class SpringServer {
+
+
+
 }
