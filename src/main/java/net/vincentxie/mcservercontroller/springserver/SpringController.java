970cd4ff4f4dbeef3c243a69fe99308fc27fa3b4
zbrachinara <zbrachinara@gmail.com>
2020-07-22 15:13:13 -0400

    Add thyme mappings

--- src/main/java/net/vincentxie/mcservercontroller/springserver/SpringController.java
+++ src/main/java/net/vincentxie/mcservercontroller/springserver/SpringController.java
@@ -10,6 +10,34 @@ import net.vincentxie.mcservercontroller.configurator.*;
 @Controller
 public class SpringController {
 
+    Server.ServerModType serverType;
+    String serverVersion;
 
+    @GetMapping("/servercreate/type")
+    public String setServerType(@RequestParam(name="serverType") String inServerType, Model model) {
+        switch (inServerType) {
+            case "Forge":
+                serverType = Server.ServerModType.FORGE;
+            case "Paper_Spigot":
+                serverType = Server.ServerModType.PAPER_SPIGOT;
+            case "Spigot":
+                serverType = Server.ServerModType.SPIGOT;
+            default:
+
+        }
+        return "/servercreate/type";
+    }
+
+    @GetMapping("/servercreate/version")
+    public String setServerVersion(@RequestParam(name="serverVersion") String inServerVersion, Model model) {
+        if (serverType == null) {
+            model.addAttribute("exception", "You did not set the server type first!");
+
+            return "/servercreate/version";
+        }
+
+
+        return "/servercreate/version";
+    }
 
 }
