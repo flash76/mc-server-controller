021b1033db4420f3e58a7e38462a53ba25f5f3cd
flash76 <30060599+flash76@users.noreply.github.com>
2020-07-22 10:11:15 -0400

    Finish Forge version scraping, add Thymeleaf to pom

--- src/main/java/net/vincentxie/mcservercontroller/configurator/MCTest.java
+++ src/main/java/net/vincentxie/mcservercontroller/configurator/MCTest.java
@@ -4,9 +4,9 @@ public class MCTest {
     public static void main(String[] args) {
         Server serverConfigurator = new Server("", Server.ServerModType.FORGE, "1.15.2");
 
-        // Tests how the version getting thing works
-//        for (String i : Versions.getVersions(Server.ServerModType.FORGE)) {
-//            System.out.println(i);
-//        }
+//         Tests how the version getting thing works
+        for (String i : Versions.getVersions(Server.ServerModType.PAPER_SPIGOT)) {
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/net/vincentxie/mcservercontroller/configurator/Versions.java
+++ src/main/java/net/vincentxie/mcservercontroller/configurator/Versions.java
@@ -6,34 +6,48 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Versions {
     public static String[] getVersions(Server.ServerModType forVersion) {
         Document docVersion;
-        String[] finalVer = null;
+        ArrayList<String> finalVers = new ArrayList<>();
         try {
             switch (forVersion) {
                 case FORGE: {
                     docVersion = Jsoup.connect("https://files.minecraftforge.net").get();
                     Elements selected = docVersion.select("li.li-version-list");
+                    for (Element element : selected) {
+                        String[] tmp = element.select("a[href]").text().split(" ");
+                        for (String i : Arrays.copyOfRange(tmp, 1, tmp.length)) {
+                            finalVers.add(i); }
+                    }
+                    break;
+                }
+                case PAPER_SPIGOT: {
+                    docVersion = Jsoup.connect("https://papermc.io/downloads").get();
+                    Elements selected = docVersion.select("li.tab");
                     StringBuilder versionString = new StringBuilder();
                     for (Element element : selected) {
                         versionString.append(element.select("a[href]").text()).append(" ");
                     }
 
-                    finalVer = versionString.toString().split(" ");
-                }
-                case PAPER_SPIGOT: {
-                    docVersion = Jsoup.connect("https://papermc.io/downloads").get();
+//                    finalVer = versionString.toString().split(" ");
+                    break;
                 }
                 case SPIGOT: {
-
+                    break;
+                }
+                default: {
+                    finalVers.add("No server software specified.");
+                    break;
                 }
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        return finalVer;
+        return finalVers.toArray(new String[finalVers.size()]);
     }
 }
